b80c68bb1891d9f3331c0581eb3bebfe3fa4c64a
Koemhong168 <suykoemhong10@gmail.com>
2025-01-02 12:11:45 +0700

    Introduction to PHP

---
+++ Introduction-php.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Introduction</title>
+</head>
+<body>
+    <?php
+        echo '<div class ="container m-auto mt-3">';
+        echo '<h1 class="col-9 text-center border-bottom">Introduction Myself</h1>';
+        echo '<h4 class="mt-4">Name : Suy Koemhong</h4>';
+        echo "<h4>Sex : Male</h4>";
+        echo "<h4>Email: suykoemhong10@gmail.com</h4>";
+        echo "<h4>Phone number: 069436260</h4>";
+        echo "<h4>University: RUPP</h4>";
+        echo "<h4>Year: 2</h4>";
+        echo "<h4>Major: ITE</h4>";
+        echo '</div>';
+    ?>
+</body>
+</html>
